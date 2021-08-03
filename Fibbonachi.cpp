677de6fc3953aea3700d0554d80d2a5157898b40
AyazSung <88406440+AyazSung@users.noreply.github.com>
2021-08-04 00:33:09 +0300

    Add files via upload

---
+++ Fibbonachi.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int rec_Fib(int f)
+{
+    if (f < 3) { return 1; }
+    return rec_Fib(f - 1) + rec_Fib(f - 2);
+}
+
+int simple_Fib(int f) {
+    if (f < 3) { return 1; }
+    int cur = 1;
+    int prev = 1;
+    
+    for (int i = 3; i <= f; i++) {
+        int t = cur;
+        cur = cur + prev;
+        prev = t;
+    }
+    return cur;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    cout << rec_Fib(n) <<"***"<< simple_Fib(n);
+}
---
+++ Spyral.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+using namespace std;
+
+void Pprint(int**a, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+			cout << '-' << a[i][j] <<'-';
+
+		cout << '\n';
+	}
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	int k = 1;
+	int lvl = 0;
+
+	int**a = new int*[n];
+	for (int i = 0; i < n; i++) {
+		a[i] = new int[n];
+	}
+	
+	while (k <= (n * n)) {
+		//идём вниз
+		for (int i = lvl; i <= n - lvl - 1 ; i++) {
+			a[i][lvl] = k;
+			k++;
+		}
+		//идём направо
+		for (int i = lvl + 1; i <= n - lvl - 1; i++) {
+			a[n-lvl-1][i] = k;
+			k++;
+		}
+		//идём вверх
+		for (int i = n - 1 - lvl -1; i >= lvl; i--) {
+			a[i][n-lvl-1] = k;
+			k++;
+		}
+		//идём налево
+		for (int i = n - lvl - 1 - 1; i >= lvl + 1; i--) {
+			a[lvl][i] = k;
+			k++;
+		}
+		lvl++;
+	}
+
+	Pprint(a, n);
+
+
+
+	
+
+
+	//очистка памяти
+	for (int i = 0; i < n; i++)
+		delete a[i];
+	delete []a;
+}
